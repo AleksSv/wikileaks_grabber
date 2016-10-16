c4c86316dad343f4ceb2c2fb6e1dedc55cd76035
Aleks S <sverdlovsa@gmail.com>
2016-10-15 20:09:11 -0500

    prevent too many threads

--- wikileaks.py
+++ wikileaks.py
@@ -1,10 +1,13 @@
 import os
 import sys, getopt
+from queue import Queue
 from threading import *
 
 from urllib.request import urlopen
 
 def main(argv):
+
+    num_worker_threads = 4
 
     start = 1
     end = 10
@@ -24,43 +27,59 @@ def main(argv):
         elif opt in ("-e", "--end"):
             end = int(arg)
 
+    ow_input = input("Do you want to overwrite emails? y/n")
+    ow = False
+    if(ow_input == "y"):
+        print("Overwriting Emails")
+        ow = True
+    else:
+        print("Downloading new files only")
+
+
     print("Grabbing emails from " + str(start) + " to " + str(end))
 
     directory = "podesta"
-    urls = []
 
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    q = Queue()
+
     for i in range(start, end + 1):
-
-        urls.append(["https://wikileaks.org/podesta-emails//get/" + str(i),str(i) + ".eml"])
-
-        # u = urlopen("https://wikileaks.org/podesta-emails//get/" + str(i))
-        # filename =
-        # localFile = open(directory + "/" + filename, 'wb')
-        # localFile.write(u.read())
-        # localFile.close()
-
-
+        q.put(["https://wikileaks.org/podesta-emails//get/" + str(i),str(i) + ".eml"])
 
     class worker(Thread):
-        def __init__(self, link, filename):
+        def __init__(self):
             Thread.__init__(self)
-            self.link = link
-            self.filename = filename
             self.start()
         def run(self):
-            u = urlopen(self.link)
-            localFile = open(directory + "/" + self.filename, 'wb')
+            while True:
+                item = q.get()
+                self.download(item[0],item[1])
+                q.task_done()
+
+        def download(self,link,filename):
+            if(os.path.isfile(directory + "/" + filename)):
+                if(ow):
+                    print("Overwriting " + filename)
+                else:
+                    print("Ignoring " + filename + " because it already exists")
+                    return
+
+            u = urlopen(link)
+            localFile = open(directory + "/" + filename, 'wb')
             localFile.write(u.read())
             localFile.close()
-            print("Downloaded " + self.filename)
+            print("Downloaded " + filename)
 
-    for url in urls:
-        worker(url[0],url[1])
 
+    for i in range(num_worker_threads):
+        t = Thread(target=worker)
+        t.daemon = True
+        t.start()
+
+    q.join()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
